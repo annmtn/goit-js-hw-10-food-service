03b4407571de6b6595d03e6c4b0f030dc08bcbb7
Anna Mitina <anndan5810@gmail.com>
2021-09-26 00:03:10 +0300

    Создает разметку всего меню, используя шаблон

---
+++ src/js/menu.js
@@ -0,0 +1,14 @@
+// 1. импортируем шаблон
+import menuTemplate from "../templates/menu.hbs"
+
+// 2. подключаем файл с данными для шаблона
+import dataMenu from "../data/menu.json"
+
+// 3. создаем разметку, вызываем шаблон и передаем аргументом массив даных
+const template = menuTemplate(dataMenu);
+
+// 4. получить доступ к элементу, куда будем встраивать шаблон
+const menuEl = document.querySelector('.js-menu')
+
+// 5. встраиваем полученный шаблон
+menuEl.insertAdjacentHTML('afterbegin', template)
